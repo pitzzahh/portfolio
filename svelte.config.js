a06ff2ee3e551c27d1f07a3985dc0822c19e12bf
pitzzahh <pitzzahh@gmail.com>
2026-03-10 18:22:38 +0800

    feat: add csp config

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,22 @@
 import adapter from '@sveltejs/adapter-cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = { kit: { adapter: adapter() } };
+const config = {
+	kit: {
+		adapter: adapter(),
+		csp: {
+			mode: 'auto',
+			directives: {
+				'default-src': ['self'],
+				'script-src': ['self'],
+				'worker-src': ['self', 'blob:'],
+				'style-src': ['self', 'unsafe-inline', 'https://fonts.googleapis.com'],
+				'img-src': ['self', 'data:'],
+				'font-src': ['self', 'https://fonts.gstatic.com'],
+				'connect-src': ['self']
+			}
+		}
+	}
+};
 
 export default config;

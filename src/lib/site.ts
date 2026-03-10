4532bef6dc9dd35239c338ef2bc0250e760612a1
pitzzahh <pitzzahh@gmail.com>
2026-03-11 07:55:27 +0800

    fix: use https URL for site.url

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -1,7 +1,15 @@
 export default {
-	url: 'peterjohnarao.com',
+	url: 'https://peterjohnarao.com',
 	author: 'Peter John Arao',
-	keywords: ['peter john arao', 'Peter John Arao', 'peterjohnarao', 'pjarao', 'araopj'],
+	keywords: [
+		'peter john arao',
+		'Peter John Arao',
+		'peterjohnarao',
+		'pjarao',
+		'araopj',
+		'peter arao',
+		'peterarao'
+	],
 	locale: 'en_PH',
 	title: 'Peter John Arao — Full-Stack Developer',
 	description:

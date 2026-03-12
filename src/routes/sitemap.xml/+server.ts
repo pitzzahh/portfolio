2164a818b156c38325410efa827415f92a130896
pitzzahh <pitzzahh@gmail.com>
2026-03-12 10:52:44 +0800

    feat: add sitemap.xml route

---
+++ src/routes/sitemap.xml/+server.ts
@@ -0,0 +1,40 @@
+import type { AppTypes } from '$app/types';
+import site from '$lib/site';
+import type { RequestHandler } from './$types';
+
+export const GET: RequestHandler = async () => {
+	const pages: Array<{
+		url: ReturnType<AppTypes['RouteId']>;
+		changefreq?: string;
+		priority?: string;
+	}> = [
+		{
+			url: '/',
+			changefreq: 'weekly',
+			priority: '1.0'
+		}
+	];
+
+	const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+${pages
+	.map(
+		(page) => `	<url>
+		<loc>${site.url}${page.url}</loc>
+		<changefreq>${page.changefreq}</changefreq>
+		<priority>${page.priority}</priority>
+	</url>`
+	)
+	.join('\n')}
+</urlset>`;
+
+	return new Response(sitemap.trim(), {
+		headers: {
+			'Content-Type': 'application/xml',
+			'Cache-Control': 'max-age=0, s-maxage=3600'
+		}
+	});
+};

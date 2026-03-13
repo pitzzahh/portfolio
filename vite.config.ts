f7db97819f0ec9cfa1da3aef19f9c4992a8c3f8e
pitzzahh <pitzzahh@gmail.com>
2026-03-13 09:17:47 +0800

    chore: remove unused vite plugin devtoolsvi

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
-import devtoolsJson from 'vite-plugin-devtools-json';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { sveltekitOG } from '@ethercorps/sveltekit-og/plugin';
 
-export default defineConfig({ plugins: [sveltekit(), sveltekitOG(), devtoolsJson()] });
+export default defineConfig({ plugins: [sveltekit(), sveltekitOG()] });

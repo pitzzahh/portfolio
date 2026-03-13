fa4731e66133f228ec022b2193f8f353fb88bd49
pitzzahh <pitzzahh@gmail.com>
2026-03-13 21:20:52 +0800

    chore: init vitest component testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,26 @@
+import { defineConfig } from 'vitest/config';
+import { playwright } from '@vitest/browser-playwright';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
 import { sveltekitOG } from '@ethercorps/sveltekit-og/plugin';
 
-export default defineConfig({ plugins: [sveltekit(), sveltekitOG()] });
+export default defineConfig({
+	plugins: [sveltekit(), sveltekitOG()],
+	test: {
+		expect: { requireAssertions: true },
+		projects: [
+			{
+				extends: './vite.config.ts',
+				test: {
+					name: 'client',
+					browser: {
+						enabled: true,
+						provider: playwright(),
+						instances: [{ browser: 'chromium', headless: true }]
+					},
+					include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
+					exclude: ['src/lib/server/**']
+				}
+			}
+		]
+	}
+});

5f9a927d4dc4b91bd7811e98037164689f8aa188
pitzzahh <pitzzahh@gmail.com>
2026-03-19 22:09:29 +0800

    chore: increase HSTS max-age and simplify handle hook

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,15 +1,11 @@
-import { sequence } from '@sveltejs/kit/hooks';
-
 /**
  * SvelteKit handle hook that adds the Strict-Transport-Security header to all responses.
  *
  * @type {import('@sveltejs/kit').Handle}
  */
-const handleHSTS = async ({ event, resolve }) => {
+export const handle = ({ event, resolve }) => {
 	event.setHeaders({
-		'Strict-Transport-Security': 'max-age=31536000; includeSubDomains; preload'
+		'Strict-Transport-Security': 'max-age=63072000; includeSubDomains; preload'
 	});
 	return resolve(event);
 };
-
-export const handle = sequence(handleHSTS);
